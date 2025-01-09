28144e9c68e60c946e3c98c3108b815788684c6b
__hemmy <haiman4real@gmail.com>
2025-01-09 07:41:35 -0500

    `Added self-service booking feature: updated database configuration, added new route and navigation link.`

--- config/database.php
+++ config/database.php
@@ -124,6 +124,21 @@ return [
             'engine' => null,
         ],
 
+        'mysql_bookings' => [
+            'driver' => 'mysql',
+            'host' => env('BK_LARAVEL_DB_HOST', '127.0.0.1'),
+            'port' => env('BK_LARAVEL_DB_PORT', '3306'),
+            'database' => env('BK_LARAVEL_DB_DATABASE', 'forge'),
+            'username' => env('BK_LARAVEL_DB_USERNAME', 'forge'),
+            'password' => env('BK_LARAVEL_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
