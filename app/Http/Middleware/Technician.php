ba4c529da63cd2d7c911666c912e7abf24309b11
__hemmy <haiman4real@gmail.com>
2025-02-17 01:15:44 -0500

    feat: Integrate Log Viewer and add Technician middleware for enhanced user access control -`Added Technician middleware and routes for service booking technician user and update job functionality`

--- app/Http/Middleware/Technician.php
+++ app/Http/Middleware/Technician.php
@@ -3,9 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Auth;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class Technician
 {
@@ -16,9 +17,20 @@ class Technician
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::user()->role !== "Technician"){
-			return abort('403', 'You do not have access to the Event Admin routes');
-		}
+        // if(Auth::user()->role !== "Technician"){
+		// 	return abort('403', 'You do not have access to the Event Admin routes');
+		// }
+        // Log the current route and user
+        Log::info('Route: ' . $request->url());
+        Log::info('User: ' . (Auth::check() ? Auth::user()->email : 'Guest'));
+
+        // return $next($request);
+        // Check if the user does not have the Technician role
+        if (Auth::check() && !in_array(strtolower(trim(Auth::user()->user_role)), ['technician'])) {
+            Log::info('User does not have access to Technician routes.');
+            return abort(403, 'You do not have access to the Technician routes');
+        }
+
         return $next($request);
     }
 }
